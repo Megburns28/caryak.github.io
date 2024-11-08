829a488a9d152c82ee1e5e28730941ed4a23660e
trevorxarcieri <agent.steel.ta@gmail.com>
2024-11-08 10:06:35 -0500

    Updated header ordering.

--- client/src/header/Header.tsx
+++ client/src/header/Header.tsx
@@ -4,17 +4,14 @@ import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
 const Header = () => (
     <AppBar position='static'>
         <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Box sx={{ display: 'flex', gap: 20 }}>
-                <IconButton
-                    size='large'
-                    aria-label='account of current user'
-                    aria-controls='menu-appbar'
-                    aria-haspopup='true'
-                    // onClick={handleMenu}
-                    color='inherit'
-                >
-                    <Leaderboard />
-                </IconButton>
+            <Box
+                sx={{
+                    display: 'flex',
+                    gap: 20,
+                    justifyContent: 'center',
+                    flex: 1,
+                }}
+            >
                 <IconButton
                     size='large'
                     aria-label='account of current user'
@@ -33,9 +30,19 @@ const Header = () => (
                     // onClick={handleMenu}
                     color='inherit'
                 >
-                    <AccountCircle />
+                    <Leaderboard />
                 </IconButton>
             </Box>
+            <IconButton
+                size='large'
+                aria-label='account of current user'
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+                // onClick={handleMenu}
+                color='inherit'
+            >
+                <AccountCircle />
+            </IconButton>
         </Toolbar>
     </AppBar>
 );

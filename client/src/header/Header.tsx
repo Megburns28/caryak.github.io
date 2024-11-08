e3c6b13ffa1d6a02074dc69eef9452e24bd518cc
trevorxarcieri <agent.steel.ta@gmail.com>
2024-11-08 10:04:22 -0500

    Added top toolbar, created some initial files for other parts of the UI.

---
+++ client/src/header/Header.tsx
@@ -0,0 +1,43 @@
+import { AccountCircle, Home, Leaderboard } from '@mui/icons-material';
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
+
+const Header = () => (
+    <AppBar position='static'>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Box sx={{ display: 'flex', gap: 20 }}>
+                <IconButton
+                    size='large'
+                    aria-label='account of current user'
+                    aria-controls='menu-appbar'
+                    aria-haspopup='true'
+                    // onClick={handleMenu}
+                    color='inherit'
+                >
+                    <Leaderboard />
+                </IconButton>
+                <IconButton
+                    size='large'
+                    aria-label='account of current user'
+                    aria-controls='menu-appbar'
+                    aria-haspopup='true'
+                    // onClick={handleMenu}
+                    color='inherit'
+                >
+                    <Home />
+                </IconButton>
+                <IconButton
+                    size='large'
+                    aria-label='account of current user'
+                    aria-controls='menu-appbar'
+                    aria-haspopup='true'
+                    // onClick={handleMenu}
+                    color='inherit'
+                >
+                    <AccountCircle />
+                </IconButton>
+            </Box>
+        </Toolbar>
+    </AppBar>
+);
+
+export default Header;

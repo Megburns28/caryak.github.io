4ef111fe3bb7de401e6ea6d4c37528af9ad99351
trevorxarcieri <agent.steel.ta@gmail.com>
2024-11-08 11:54:45 -0500

    Fixed rendering issue in header on menu close.

--- client/src/header/Header.tsx
+++ client/src/header/Header.tsx
@@ -1,50 +1,112 @@
 import { AccountCircle, Home, Leaderboard } from '@mui/icons-material';
-import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
+import {
+    AppBar,
+    Box,
+    IconButton,
+    Menu,
+    MenuItem,
+    Toolbar,
+} from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import {
+    ChangeEvent,
+    MouseEvent,
+    useCallback,
+    useEffect,
+    useState,
+} from 'react';
 
-const Header = () => (
-    <AppBar position='static'>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Box
-                sx={{
-                    display: 'flex',
-                    gap: 20,
-                    justifyContent: 'center',
-                    flex: 1,
-                }}
-            >
-                <IconButton
-                    size='large'
-                    aria-label='account of current user'
-                    aria-controls='menu-appbar'
-                    aria-haspopup='true'
-                    // onClick={handleMenu}
-                    color='inherit'
+const Header = () => {
+    const [auth, setAuth] = useState(true);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const [closingMenu, setClosingMenu] = useState(false);
+
+    const handleAuth = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        setAuth(event.target.checked);
+    }, []);
+
+    const handleMenu = useCallback((event: MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    }, []);
+
+    const handleClose = useCallback(() => {
+        setClosingMenu(true);
+    }, []);
+
+    const theme = useTheme();
+
+    useEffect(() => {
+        let timeout = 0;
+        if (closingMenu) {
+            timeout = setTimeout(() => {
+                setAnchorEl(null);
+                setClosingMenu(false);
+            }, theme.transitions.duration.leavingScreen);
+        }
+
+        return () => {
+            clearTimeout(timeout);
+        };
+    });
+
+    return (
+        <AppBar position='static'>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        gap: 30,
+                        justifyContent: 'center',
+                        flex: 1,
+                    }}
                 >
-                    <Home />
+                    <IconButton
+                        size='large'
+                        // onClick={handleMenu}
+                        color='inherit'
+                    >
+                        <Home />
+                    </IconButton>
+                    <IconButton
+                        size='large'
+                        // onClick={handleMenu}
+                        color='inherit'
+                    >
+                        <Leaderboard />
+                    </IconButton>
+                </Box>
+                <IconButton size='large' onClick={handleMenu} color='inherit'>
+                    <AccountCircle />
                 </IconButton>
-                <IconButton
-                    size='large'
-                    aria-label='account of current user'
-                    aria-controls='menu-appbar'
-                    aria-haspopup='true'
-                    // onClick={handleMenu}
-                    color='inherit'
+                <Menu
+                    id='menu-appbar'
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={!closingMenu && !!anchorEl}
+                    onClose={handleClose}
                 >
-                    <Leaderboard />
-                </IconButton>
-            </Box>
-            <IconButton
-                size='large'
-                aria-label='account of current user'
-                aria-controls='menu-appbar'
-                aria-haspopup='true'
-                // onClick={handleMenu}
-                color='inherit'
-            >
-                <AccountCircle />
-            </IconButton>
-        </Toolbar>
-    </AppBar>
-);
+                    {anchorEl ? (
+                        <>
+                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={handleClose}>
+                                My account
+                            </MenuItem>
+                        </>
+                    ) : (
+                        <MenuItem onClick={handleClose}>Log in</MenuItem>
+                    )}
+                </Menu>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default Header;

fc02f3a58b9466217fd25c561e6bad5f8b7dc44d
Nicholas Learman <nicholas.learman@gmail.com>
2024-11-14 21:16:00 -0500

    Email credentials set up

---
+++ server/api/tests/authTest.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+
+from api.main import app
+
+
+client = TestClient(app)
+
+def test_read_main(user):
+    response = client.post("/api/auth/register", json=user)
+    print(response)
+
+user = {
+    "name": "John Doe",
+    "email": "email@kettering.edu",
+    "photo": "temp",
+    "password": "password123",
+    "passwordConfirm": "password123"
+}
+
+test_read_main(user)
